393bd6c148fce6dd246988a71a8f24ac48c61a7f
privet <privet56@hotmail.com>
2019-03-15 18:09:38 +0100

    tabs with iconicons

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
-//import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Colors from '../constants/Colors';
 
@@ -10,17 +10,12 @@ import Colors from '../constants/Colors';
 export default class TabBarIcon extends React.Component {
   render() {
     return (
-
-      <Text>TODO icons</Text>
-
-      /*
       <Ionicons
         name={this.props.name}
         size={26}
         style={{ marginBottom: -3 }}
         color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
       />
-      */
     );
   }
 }
